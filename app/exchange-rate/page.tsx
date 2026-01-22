7ebcf098c4b2eb32e337874bf155bbd5b9b11232
John Thomas Alog <johnthomasalog@gmail.com>
2026-01-22 13:46:33 +0800

    refactor: remove Suspense wrapper and relocate ExchangedRatesSkeleton to loading.tsx

--- app/exchange-rate/page.tsx
+++ app/exchange-rate/page.tsx
@@ -1,11 +1,7 @@
-import { Suspense } from "react";
 import ExchangedRates from "./ExchangedRates";
-import ExchangedRatesSkeleton from "@/components/skeletons/ExchangedRatesSkeleton";
 
 export default function page() {
     return (
-        <Suspense fallback={<ExchangedRatesSkeleton />}>
-            <ExchangedRates />
-        </Suspense>
+        <ExchangedRates />
     )
 }
